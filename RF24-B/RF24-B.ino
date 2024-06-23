72196e0508c9ae017a7fe299b4a5c2c9f95924f3
James Pinder <jhpinder@gmail.com>
2024-06-23 18:45:26 -0400

    rf24 library working

---
+++ RF24-B/RF24-B.ino
@@ -0,0 +1,33 @@
+#include <MIDI.h>
+#include <RF24MIDI.h>
+
+#define RF24MIDIINADDR  50
+#define RF24MIDIOUTADDR 49
+
+RF24MIDI_CREATE_INSTANCE(RF24MIDIINADDR, RF24MIDIOUTADDR, RF24MIDI);
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void setup() {
+   MIDI.begin(MIDI_CHANNEL_OMNI);
+   MIDI.turnThruOff();
+   RF24MIDI.begin(MIDI_CHANNEL_OMNI);
+   RF24MIDI.turnThruOff();
+}
+
+void loop() {
+
+  if (MIDI.read())
+  {
+     RF24MIDI.send(MIDI.getType(),
+                   MIDI.getData1(),
+                   MIDI.getData2(),
+                   MIDI.getChannel());
+  }
+  if (RF24MIDI.read())
+  {
+     MIDI.send(RF24MIDI.getType(),
+               RF24MIDI.getData1(),
+               RF24MIDI.getData2(),
+               RF24MIDI.getChannel());
+  }
+}
