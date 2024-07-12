f02a3558290a7ffdda45b78c0543fd932d630ef8
James Pinder <jhpinder@gmail.com>
2024-07-12 16:17:11 -0400

    cleaned up unused functions and vars

--- midi_processing/midi_processing.ino
+++ midi_processing/midi_processing.ino
@@ -1,36 +1,31 @@
 #include <MIDI.h>
 
-#define SWELL_CHANNEL 1
-#define GREAT_CHANNEL 2
-#define PEDAL_CHANNEL 3
-#define CHOIR_CHANNEL 4
-#define SOLO_CHANNEL 5
-#define PISTON_CHANNEL 8
+#define SWELL_CHANNEL   1
+#define GREAT_CHANNEL   2
+#define PEDAL_CHANNEL   3
+#define CHOIR_CHANNEL   4
+#define SOLO_CHANNEL    5
+#define PISTON_CHANNEL  8
+
+#define LAUNCHKEY_PISTON_CHANNEL   10
+#define LAUNCHKEY_CANCEL_NUM     0x72
+#define LAUNCHKEY_BASS_ON        0x68
+#define LAUNCHKEY_BASS_OFF       0x69
+#define LAUNCHKEY_MIN_KEY        0x33
+#define LAUNCHKEY_MAX_KEY        0x37
+#define PISTON_PRG_BASE          0x28
+
 #define CANCEL_BUTTON 0x7F
+
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-byte pistonInputPins[10] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-byte currPistonState[10] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
-byte prevPistonState[10] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
-byte pistonLampPins[10] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
-byte cancelPistonPin = 12;
-bool prevCancelState = false;
-
-byte keyboardButtonBasePin = 20;
-byte keyboardLedBasePin = 25;
 byte keyboardButtonChannels[4] = {4, 2, 1, 5};
-byte currKeyboardButtonState[4] = {LOW, LOW, LOW, LOW};
-byte prevKeyboardButtonState[4] = {LOW, LOW, LOW, LOW};
-
-byte togglePedalButton = 40;
-byte pedalStateLED = 41;
 
 bool activeNotes[128];
 byte activeMidiChannel = 2;
 byte previousMidiChannel = 2;
 bool currPedalState = true;
-bool prevPedalButtonState = false;
 int currentPedalNote = -1;
 int lastPedalNote = -1;
 bool lowestNoteWasReleased = false;
@@ -45,28 +40,19 @@ void setup() {
   MIDI.setHandleControlChange(handleCC);
   MIDI.begin(MIDI_CHANNEL_OMNI);
   MIDI.turnThruOff();
-  for (int i = 0; i < 10; i++) {
-    pinMode(pistonInputPins[i], INPUT_PULLUP);
-    pinMode(pistonLampPins[i], OUTPUT);
-  }
-  for (int i = 0; i < 4; i++) {
-    pinMode(keyboardButtonBasePin + i, INPUT_PULLUP);
-    pinMode(keyboardLedBasePin + i, OUTPUT);
-  }
-  pinMode(togglePedalButton, INPUT_PULLUP);
-  pinMode(pedalStateLED, OUTPUT);
-  pinMode(cancelPistonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  scanPistons();
-  scanButtons();
   MIDI.read();
   sendPedalNotes();
 }
 
 
 void handleNoteOn(byte channel, byte note, byte velocity) {
+  if (channel == LAUNCHKEY_PISTON_CHANNEL) {
+    MIDI.sendProgramChange(note - PISTON_PRG_BASE, PISTON_CHANNEL);
+    return;
+  }
   activeNotes[note] = true;
   MIDI.sendNoteOn(note, velocity, activeMidiChannel);
   if (lowestNoteWasReleased) {
@@ -75,12 +61,58 @@ void handleNoteOn(byte channel, byte note, byte velocity) {
 }
 
 void handleNoteOff(byte channel, byte note, byte velocity) {
+  if (channel == LAUNCHKEY_PISTON_CHANNEL) {
+    return;
+  }
   activeNotes[note] = false;
   MIDI.sendNoteOff(note, velocity, activeMidiChannel);
-  lowestNoteWasReleased = (lastPedalNote == note) && (lowestNoteOn() > note);
+  lowestNoteWasReleased = (lastPedalNote == note)
+    && (lowestNoteOn() > note)
+    && (autoPedalDisabled());
 }
 
 void handleCC(byte channel, byte number, byte value) {
+  if (number == LAUNCHKEY_CANCEL_NUM) {
+    if (value) {
+      MIDI.sendProgramChange(CANCEL_BUTTON, PISTON_CHANNEL);
+    }
+    return;
+  }
+
+  if (number == LAUNCHKEY_BASS_ON) {
+    if (value) {
+      currPedalState = true;
+      if (lowestNoteOn() > -1) {
+        MIDI.sendNoteOn(lowestNoteOn(), 64, PEDAL_CHANNEL);
+      }
+    }
+    return;
+  }
+
+  if (number == LAUNCHKEY_BASS_OFF) {
+    if (value) {
+      currPedalState = false;
+      if (lowestNoteOn() > -1) {
+        MIDI.sendNoteOff(currentPedalNote, 0, PEDAL_CHANNEL);
+      }
+    }
+    return;
+  }
+
+  if (number >= LAUNCHKEY_MIN_KEY && number < LAUNCHKEY_MAX_KEY) {
+    if (value) {
+      activeMidiChannel = keyboardButtonChannels[number - LAUNCHKEY_MIN_KEY];
+      for (int j = 0; j < 128; j++) {
+        if (activeNotes[j]) {
+          MIDI.sendNoteOn(j, 64, activeMidiChannel);
+          MIDI.sendNoteOff(j, 0, previousMidiChannel);
+        }
+      }
+      previousMidiChannel = activeMidiChannel;
+    }
+    return;
+  }
+
   MIDI.sendControlChange(number, value, activeMidiChannel);
 }
 
@@ -99,6 +131,7 @@ void sendPedalNotes() {
   }
 
   if (!currPedalState) {
+    MIDI.sendNoteOff(currentPedalNote, 0, PEDAL_CHANNEL);
     return;
   }
 
@@ -119,55 +152,19 @@ int lowestNoteOn() {
   return -1;
 }
 
-void scanPistons() {
-  for (int i = 0; i < 10; i++) {
-    currPistonState[i] = !digitalRead(pistonInputPins[i]);
-    if (currPistonState[i] != prevPistonState[i]
-    && currPistonState
-    && millis() - prevMillis > 300) {
-      prevMillis = millis();
-      for (int j = 0; j < 10; j++) {
-        digitalWrite(pistonLampPins[j], i == j);
-      }
-      MIDI.sendProgramChange(i, PISTON_CHANNEL);
+bool autoPedalDisabled() {
+  byte numNotes = 0;
+  byte largestGap = 0;
+  byte bottom = -1;
+  byte top = -1;
+  for (int i = 0; i < 128; i++) {
+    if (activeNotes[i]) {
+      top = i;
+      largestGap = top - bottom > largestGap && bottom >= 0 ? top - bottom : largestGap;
+      numNotes++;;
+      bottom = i;
     }
-    prevPistonState[i] = currPistonState[i];
   }
-}
-
-void scanButtons() {
-  for (int i = 0; i < 4; i++) {
-    currKeyboardButtonState[i] = !digitalRead(i + keyboardButtonBasePin);
-    if (currKeyboardButtonState[i] != prevKeyboardButtonState[i] && currKeyboardButtonState[i]) {
-      activeMidiChannel = keyboardButtonChannels[i];
-      if (currentPedalNote > -1) {
-        for (int j = 0; j < 128; j++) {
-          if (activeNotes[j]) MIDI.sendNoteOff(j, 0, previousMidiChannel);
-        }
-      }
-    }
-    digitalWrite(i + keyboardLedBasePin, activeMidiChannel == keyboardButtonChannels[i]);
-  }
-
-  if (!digitalRead(togglePedalButton)
-    && !prevPedalButtonState
-    && millis() - prevMillis > 100) {
-    prevMillis = millis();
-    currPedalState = !currPedalState;
-    digitalWrite(pedalStateLED, currPedalState);
-    prevPedalButtonState = true;
-  } else {
-    prevPedalButtonState = false;
-  }
-
-  if (!digitalRead(cancelPistonPin)
-    && !prevCancelState
-    && millis() - prevMillis > 100) {
-    MIDI.sendProgramChange(CANCEL_BUTTON, PISTON_CHANNEL); // send cancel button
-    prevMillis = millis();
-    prevCancelState = true;
-  } else {
-    prevCancelState = false;
-  }
+  return numNotes > 2 && largestGap < 5;
 }
 
